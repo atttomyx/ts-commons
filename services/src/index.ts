64d75ec4244e411bd1fc2a1d85da393e20e1a3f9
realiteer <mikedaniels1546@gmail.com>
2025-10-10 20:55:15 -0700

    added AccountUtils type to the index

--- services/src/index.ts
+++ services/src/index.ts
@@ -1,6 +1,20 @@
-import {type AuthUser, type LoginResponse, type RecoverResponse, type Login, type LoginList, authService} from "./authService";
-import {type Account, type Branding, type Join, type AccountUser, accountService} from "./accountService";
-import {type Profile, type User, type UserUtils, type UserList, userService} from "./userService";
+import {
+    authService,
+    type AuthUser,
+    type Login,
+    type LoginList,
+    type LoginResponse,
+    type RecoverResponse
+} from "./authService";
+import {
+    type Account,
+    accountService,
+    type AccountUser,
+    type AccountUtils,
+    type Branding,
+    type Join
+} from "./accountService";
+import {type Profile, type User, type UserList, userService, type UserUtils} from "./userService";
 import {type Type, type TypeList, typeService} from "./typeService";
 import {cloudinaryService} from "./cloudinaryService";
 
@@ -15,13 +29,14 @@ export {Account};
 export {Branding};
 export {Join};
 export {AccountUser};
+export {AccountUtils};
 export {accountService};
 
 // users
 export {Profile};
 export {User};
-export {UserUtils};
 export {UserList};
+export {UserUtils};
 export {Login};
 export {LoginList};
 export {userService};
